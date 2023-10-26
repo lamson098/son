1a3d712536e65e4254614a9465280c6508d2f69f
nguyen lam son <sonxautrai2@gmail.com>
2023-10-26 11:27:28 +0700

    first commit

---
+++ src/Miniature.js
@@ -0,0 +1,164 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from 'react-bootstrap/Card';
+import AOS from "aos";
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
+import Miniature from './Miniature.json'
+import { Link } from 'react-router-dom';
+
+
+
+const Miniature1 = () => {
+
+    useEffect(() => {
+        AOS.init({ duration: 2000 })
+
+    }, [])
+
+    return (
+        <>
+            <div className="nav">
+                <Navbar className="bg-body-tertiary " fixed="top" expand="lg">
+                    <Container fluid className="nav">
+
+                        <Navbar.Brand href="/"><span className="logo">Garden World</span></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+
+
+                        <Navbar.Collapse id="navbarScroll" float="right">
+                            <Nav
+                                className="me-auto my-2 my-lg-0 nav2"
+                                style={{ maxHeight: '500px' }}
+                                navbarScroll
+
+                            >
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="#action2">Gardening Tips</Nav.Link>
+                                <NavDropdown title="Products" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item href="/Product">All Product</NavDropdown.Item>
+                                    <NavDropdown.Item href="/Product/Soilfertilizers">Soil fertilizers</NavDropdown.Item>
+                                    <NavDropdown.Item href="/Product/Pesticides">
+                                        Pesticides for plants
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="/Product/Seeds">
+                                        Seeds
+                                    </NavDropdown.Item>
+                                    <NavDropdown title="Plant pots" id="navbarScrollingDropdown">
+                                        <NavDropdown.Item href="/Product/Pots">
+                                            Pots
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Potplates">
+                                            Pot plates
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Wallmounted">
+                                            Wall mounted
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Wallhangingbracket">
+                                            Wall hanging brackets
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Pothangers">
+                                            Pot hangers
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Railingbrackets">
+                                            Railing brackets
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                    <NavDropdown title="Accessories" id="navbarScrollingDropdown">
+                                        <NavDropdown.Item href="/Product/Miniature">
+                                            Miniature garden toys
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Accessories">
+                                            Gardening accessories
+
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Decorative">
+                                            Decorative pebbles
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </NavDropdown>
+
+                            </Nav>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+
+
+                    </Container>
+                </Navbar>
+            </div>
+
+            <div className="product-main">
+                <div className="product">
+                    <div className="title">
+                        <p className='mediumtext boldtext'> Miniature Garden Toys</p>
+                    </div>
+                    <div className="product1 aninmation" data-aos='fade-up'>
+                        {Miniature.map((item) => {
+
+                            return <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={require(`${item.picture}`)} />
+                                {console.log(item.picture)}
+                                <Card.Body>
+                                    <Card.Title>{item.name}</Card.Title>
+                                    <Card.Text>
+                                        {item.price}
+                                    </Card.Text>
+                                    <Button variant="primary" className='me-3'>Add To Cart</Button>
+
+                                    <Link to={`/Product/${item.id}`}><Link to={`/Product/${item.id}`}><Button variant="warning">See Detail</Button></Link></Link>
+                                </Card.Body>
+                            </Card>
+
+                        })}
+
+
+
+                    </div>
+                </div>
+
+                <div className="footer">
+                    <div className="footer-logo">
+                        <p className="greentext boldtext bigtext">Garden World</p>
+                    </div>
+                    <div className="whitetext footer-contain">
+                        <div className="footer-contact">
+                            <p className="boldtext mediumtext">Get in touch</p>
+                            <p>14 Tottenham Road, N1 4EP
+                                London, United Kingdom</p>
+                            <p>00419-306-2667</p>
+                            <p>iamsteelthemes@gmail.com</p>
+                        </div>
+                        <div className="footer-timework">
+                            <p className="boldtext mediumtext">Work time</p>
+                            <p>Mon - Sat 8.00 - 18.00
+                                Sunday - OPEN</p>
+                            <p>Mon - Sat 8.00 - 18.00
+                                Sunday - OPEN</p>
+                            <p>Mon - Sat 8.00 - 18.00
+                                Sunday - OPEN</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default Miniature1;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import './style.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Product from './Product';
+import Soilfertilizers from './Soilfertilizers'
+import Pesticides from './Pesticides'
+import Seeds from './Seeds'
+import Pots from './Pots'
+import Potplates from './Potplates'
+import Wallmounted from './Wallmounted'
+import Wallhangingbracket from './Wallhangingbracket'
+import Pothangers from './Pothangers'
+import Railingbrackets from './Railingbrackets'
+import Miniature from './Miniature'
+import Accessories from './Accessories'
+import Decorative from './Decorative'
+import Detail from './Detail'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Product' element={<Product />} />
+        <Route path='/Product/Soilfertilizers' element={<Soilfertilizers />} />
+        <Route path='/Product/Pesticides' element={<Pesticides />} />
+        <Route path='/Product/Seeds' element={<Seeds />} />
+        <Route path='/Product/Pots' element={<Pots />} />
+        <Route path='/Product/Potplates' element={<Potplates />} />
+        <Route path='/Product/Wallmounted' element={<Wallmounted />} />
+        <Route path='/Product/Wallhangingbracket' element={<Wallhangingbracket />} />
+        <Route path='/Product/Pothangers' element={<Pothangers />} />
+        <Route path='/Product/Railingbrackets' element={<Railingbrackets />} />
+        <Route path='/Product/Miniature' element={<Miniature />} />
+        <Route path='/Product/Accessories' element={<Accessories />} />
+        <Route path='/Product/Decorative' element={<Decorative />} />
+        <Route path='/Product/:id' element={<Detail />} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
