962f8fe3d601bab2bb22d10e6323ea420b98ce2f
lamson098 <145956509+lamson098@users.noreply.github.com>
2023-10-31 19:59:44 +0700

    Add files via upload

---
+++ src/Search.js
@@ -0,0 +1,184 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import AOS from "aos";
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
+import Detaildata from './Detaildata.json'
+import { useParams } from 'react-router';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const Accessories1 = () => {
+    let { name } = useParams();
+
+    useEffect(() => {
+        AOS.init({ duration: 2000 })
+
+    }, [])
+
+    const [search, setsearch] = useState("")
+    const searchbox = (event) => {
+        setsearch(event.target.value)
+    }
+
+
+    return (
+        <>
+            <div className="nav">
+
+                <Navbar className="bg-body-tertiary " fixed="top" expand="lg">
+                    <Container fluid className="nav">
+
+                        <Navbar.Brand href="/"><span className="logo">Garden World</span></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+
+
+                        <Navbar.Collapse id="navbarScroll" float="right">
+                            <Nav
+                                className="me-auto my-2 my-lg-0 nav2"
+                                style={{ maxHeight: '500px' }}
+                                navbarScroll
+
+                            >
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/Gardentips">Gardening Tips</Nav.Link>
+                                <NavDropdown title="Products" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item href="/Product">All Product</NavDropdown.Item>
+                                    <NavDropdown.Item href="/Product/Soilfertilizers">Soil fertilizers</NavDropdown.Item>
+                                    <NavDropdown.Item href="/Product/Pesticides">
+                                        Pesticides for plants
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="/Product/Seeds">
+                                        Seeds
+                                    </NavDropdown.Item>
+                                    <NavDropdown title="Plant pots" id="navbarScrollingDropdown">
+                                        <NavDropdown.Item href="/Product/Pots">
+                                            Pots
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Potplates">
+                                            Pot plates
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Wallmounted">
+                                            Wall mounted
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Wallhanging">
+                                            Wall hanging brackets
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Pothangers">
+                                            Pot hangers
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Railingbrackets">
+                                            Railing brackets
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                    <NavDropdown title="Accessories" id="navbarScrollingDropdown">
+                                        <NavDropdown.Item href="/Product/Miniature">
+                                            Miniature garden toys
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Accessories">
+                                            Gardening accessories
+
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="/Product/Decorative">
+                                            Decorative pebbles
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </NavDropdown>
+
+                            </Nav>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                    value={search}
+                                    onChange={(event) => { searchbox(event) }}
+                                />
+                                <Link to={`/Product/search/${search}`}><Button variant="outline-success">Search</Button></Link>
+                            </Form>
+                        </Navbar.Collapse>
+
+
+                    </Container>
+                </Navbar>
+            </div>
+
+            <div className="product-main">
+                <div className="product">
+                    <div className="title">
+                        <p className='mediumtext boldtext'></p>
+                    </div>
+                    <div className="product1 aninmation" data-aos='fade-up'>
+                        {Detaildata.map((item) => {
+                            return item.name.toLowerCase().includes(name.toLowerCase()) ? <div className='all-details' key={Math.floor(Math.random() * 10000)}>
+                                <div className='top-content-details'>
+                                    <div className='image-details'>
+                                        <img src={require(`${item.picture}`)} alt='name' style={{ width: '80%' }}></img>
+                                    </div>
+                                    <div className='content-details'>
+                                        <div className='name-details'>
+                                            <h1>{item.name}</h1>
+                                            <p>{item.code}</p>
+                                        </div>
+                                        <div className='price-details'>
+                                            <h2>{item.price}</h2>
+                                        </div>
+                                        <div>
+                                            <p>Ships directly from manufacturer - departs in about 3 weeks</p>
+                                        </div>
+                                        <Button variant="primary" className='me-3'>Add To Cart</Button>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p>{item.desc}</p>
+                                </div>
+                            </div> : []
+                        })}
+                    </div>
+
+
+
+                </div>
+
+                <div className="footer">
+                    <div className="footer-logo">
+                        <p className="greentext boldtext bigtext">Garden World</p>
+                    </div>
+                    <div className="whitetext footer-contain">
+                        <div className="footer-contact">
+                            <p className="boldtext mediumtext">Get in touch</p>
+                            <p>14 Tottenham Road, N1 4EP
+                                London, United Kingdom</p>
+                            <p>00419-306-2667</p>
+                            <p>iamsteelthemes@gmail.com</p>
+                        </div>
+                        <div className="footer-timework">
+                            <p className="boldtext mediumtext">Work time</p>
+                            <p>Mon - Sat 8.00 - 18.00
+                                Sunday - OPEN</p>
+                            <p>Mon - Sat 8.00 - 18.00
+                                Sunday - OPEN</p>
+                            <p>Mon - Sat 8.00 - 18.00
+                                Sunday - OPEN</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default Accessories1;
